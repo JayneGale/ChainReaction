7da431122142d2495dd699938d44eee6c609a504
Jayne <jaynegale@gmail.com>
2019-07-20 17:49:09 +1200

    Killbox with Audio

--- New Unity Project/Assets/Scripts/AudioManager.cs
+++ New Unity Project/Assets/Scripts/AudioManager.cs
@@ -54,4 +54,18 @@ public class AudioManager : MonoBehaviour
         s.source.Play();
     }
 
+    public void PlayOneShot (string sound)
+    {
+        Sound s = Array.Find(sounds, item => item.name == sound);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+
+        s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
+        s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
+        s.source.PlayOneShot(s.clip);
+    }
+
 }
---
+++ New Unity Project/Assets/Scripts/KillBox.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class KillBox : MonoBehaviour
+{
+    
+    int goldLost;
+    int silverLost;
+    int copperLost;
+    AudioManager AM;
+    AudioSource AMSource;
+
+    private void Start()
+    {
+        goldLost = 0;
+        silverLost = 0;
+        copperLost = 0;
+        AM = FindObjectOfType<AudioManager>();
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+ //       Debug.Log("Collider tag = " + col.gameObject.tag);
+
+        if (col.gameObject.CompareTag("Gold"))
+        {
+            goldLost += 1;          
+            AM.PlayOneShot("GoldFallsOffscreen");
+        }
+        if (col.gameObject.CompareTag("Silver"))
+        {
+            silverLost += 1;
+            AM.PlayOneShot("LinkFallsOffscreen");
+        }
+        if (col.gameObject.CompareTag("Copper"))
+        {
+            copperLost += 1;
+           AM.PlayOneShot("LinkFallsOffscreen");
+        }
+        Destroy(col.gameObject);
+
+        /*        when the game restarts, reset the lost links to zero
+        if (Play Again)
+                {
+                goldLost = 0;
+                silverLost = 0;
+                copperLost = 0;
+                }
+        */
+    }
+    public List<int> LostLinkCount() {
+        var list = new List<int> { goldLost, silverLost, copperLost};
+        return list;
+    }
+
+}
