b6efe98f120612f48a4aaa7569615989741dff23
Jayne <jaynegale@gmail.com>
2019-07-20 13:10:11 +1200

    Scripts added

---
+++ New Unity Project/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public KeyCode PauseKey;
+    public GameObject pauseMenu;
+    public void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(PauseKey))
+        {
+            pauseMenu.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Time.timeScale = 0;
+        }
+    }
+
+    public void GameResume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(0);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 0;
+    }
+
+    public void ReallyQuit()
+    {
+        Application.Quit();
+    }
+
+}
