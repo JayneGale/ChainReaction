e12300b47b7e597c8ae2640450bc005e896a9e43
Jayne Gale <GameDev1*>
2019-07-20 15:55:54 +1200

    Auto stash before merge of "master" and "origin/master"

---
+++ New Unity Project/Assets/RotatingChainIdea/Scripts/CollisionDetect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetect : MonoBehaviour
+{
+    bool fired = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!fired)
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            transform.parent = collision.transform;
+            fired = true;
+        }
+
+    }
+}
---
+++ New Unity Project/Assets/RotatingChainIdea/Scripts/GunLogic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunLogic : MonoBehaviour
+{
+
+    public List<GameObject> links;
+    public Vector3 hitStrength = new Vector3(0,-400,0);
+    GameObject currentLink;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateNewLink();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            FireLink();
+            GenerateNewLink();
+        }
+    }
+
+    void FireLink()
+    {
+        currentLink.GetComponent<Rigidbody>().AddForce(transform.rotation * hitStrength);
+    }
+
+    void GenerateNewLink()
+    {
+        GameObject link = links[Random.Range(0,links.Count)];
+        currentLink = Instantiate(link,transform,false);
+    }
+}
